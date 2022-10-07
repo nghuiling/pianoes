f1b35cd739f4b832ece31387ee1564e0d21770a6
nghuiling <lotta.luv@hotmail.com>
2022-10-07 23:21:59 +0800

    added docker files,and Test.js to link Flask server to React  client

---
+++ client/src/Components/Test.js
@@ -0,0 +1,11 @@
+// testing link to flask server
+import React from 'react';
+
+export const Test = ({data})=> {
+    return(
+        <div>
+            <h1>{data.name}</h1>
+            <h1>{data.test}</h1>
+        </div>
+    )
+}

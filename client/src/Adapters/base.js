c4f5969904b826a7cf71caeee1d4803625b8bbde
jon <cysjonathan@gmail.com>
2022-10-10 13:40:37 +0800

    abstract api fetch calls enforce single quotes on prettier

---
+++ client/src/Adapters/base.js
@@ -0,0 +1,15 @@
+const base_url = process.env.REACT_APP_API_URI;
+
+export function get(path) {
+  path = path || '';
+  return fetch(base_url + path, { method: 'GET' })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      throw new Error('Something went wrong');
+    })
+    .catch((error) => {
+      return { status: 400 };
+    });
+}
--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,62 @@
-import "./App.css";
-import ResponsiveAppBar from "./Components/ResponsiveAppBar";
-import Carousel from "./Components/Carousel";
-import Container from "@mui/material/Container";
-import Stack from "@mui/material/Stack";
-import { Typography } from "@mui/material";
-import Grid from "@mui/material/Grid";
+import './App.css';
+import ResponsiveAppBar from './Components/ResponsiveAppBar';
+import Carousel from './Components/Carousel';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
 
-import {useState, useEffect} from 'react';
-import { Test } from './Components/Test'
+import { useState, useEffect } from 'react';
+import { Test } from './Components/Test';
+import { get } from './Adapters/base';
 
 function App() {
-
-
-
   // // added this for testing
   // // start of test
-  const [initialState, setState] = useState([])
+  const [initialState, setState] = useState([]);
 
-  const url = '/api'
-
-  useEffect(()=> {
-    fetch(url).then(response => {
-      if(response.status === 200){
-        return response.json()
-      }
-    }).then(data => setState(data))
-  }, [])
+  useEffect(() => {
+    get().then((data) => setState(data));
+  }, []);
   // // end of test
-
 
   return (
     <>
-    {/* testing start */}
-    <div className="App">
-      <header>
-        <Test data={initialState}/>
-      </header>
-    </div>
+      {/* testing start */}
+      <div className='App'>
+        <header>
+          <Test data={initialState} />
+        </header>
+      </div>
       {/* testing end */}
-    <Stack
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      sx={{ height: "100vh" }}
-    >
-      <ResponsiveAppBar />
-      <Container
-        maxWidth="lg"
-        sx={{
-          flexGrow: 1,
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-        }}
+      <Stack
+        direction='column'
+        justifyContent='center'
+        alignItems='center'
+        sx={{ height: '100vh' }}
       >
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Typography variant="h5" sx={{ textAlign: "center" }}>
-              Select a sheet music to play
-            </Typography>
+        <ResponsiveAppBar />
+        <Container
+          maxWidth='lg'
+          sx={{
+            flexGrow: 1,
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Typography variant='h5' sx={{ textAlign: 'center' }}>
+                Select a sheet music to play
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Carousel />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Carousel />
-          </Grid>
-        </Grid>
-      </Container>
-    </Stack>
+        </Container>
+      </Stack>
     </>
   );
 }
